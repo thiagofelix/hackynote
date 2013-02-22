ccfdd820593de9e3e5817b40ad76e3e2df17d304
thiagofelix <felixthi@gmail.com>
2013-02-22 10:13:48 -0300

    Menu Themes
    First commit of menu with themes and transitions.
    Very beta version
    
    Signed-off-by: thiagofelix <felixthi@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -1,5 +1,5 @@
 // Declare app level module which depends on filters, and services
-angular.module('hn', ['hn.filters', 'hn.services', 'hn.directives', 'hn.controllers', 'ngSanitize']).
+angular.module('hn', ['hn.filters', 'hn.services', 'hn.directives', 'hn.controllers', 'ngSanitize', 'ui.bootstrap']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/project', {templateUrl: 'partials/page/project.html', controller: "ProjectCtrl"});
     $routeProvider.otherwise({redirectTo: '/project'});
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -9,5 +9,54 @@ angular.module('hn.controllers', []).
         markdown: response.data
       };
     });
+  }]).
+  controller('ThemesCtrl',['$scope', function($scope){
+    $scope.setTheme = function(theme){
+      $scope.theme.current = theme;
+      $('#style-theme-link').attr('href', theme.src);
+    };
 
+    $scope.theme = {};
+    $scope.theme.list = [{
+      src: "themes/dclick-1.0.css",
+      name: "DClick"
+    },{
+      src: "lib/deck.js/themes/style/web-2.0.css",
+      name: "Web 2.0"
+    },{
+      src: "lib/deck.js/themes/style/swiss.css",
+      name: "Swiss"
+    },{
+      src: "lib/deck.js/themes/style/neon.css",
+      name: "Neon"
+    },{
+      src: "",
+      name: "None"
+    }];
+
+    $scope.setTheme($scope.theme.list[0]);
+
+  }]).
+  controller('TransitionsCtrl',['$scope', function($scope){
+    $scope.setTransition = function(transition){
+      $scope.transition.current = transition;
+      $('#transition-theme-link').attr('href', transition.src);
+    };
+
+    $scope.transition = {};
+    $scope.transition.list = [{
+      src: "lib/deck.js/themes/transition/horizontal-slide.css",
+      name: "Horizontal Slide"
+    },{
+      src: "lib/deck.js/themes/transition/vertical-slide.css",
+      name: "Vertical Slide"
+    },{
+      src: "lib/deck.js/themes/transition/fade.css",
+      name: "Fade"
+    },{
+      src: "",
+      name: "None"
+    }];
+
+    $scope.setTransition($scope.transition.list[0]);
   }]);
