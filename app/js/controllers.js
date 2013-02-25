9cda8fb63825190ad59e4e22f7c8884363816fcf
thiagofelix <felixthi@gmail.com>
2013-02-25 14:46:08 -0300

    stop initialize controller with a example markdown
    Previous the ProjectCtrl was requesting a markdown from examples's folder just to demonstration purpose on the beta version.
    
    It's is not desired to the controller continue doing such thing.
    
    Signed-off-by: thiagofelix <felixthi@gmail.com>

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,11 +4,7 @@ angular.module('hn.controllers', []).
    * Project Controller
    */
   controller('ProjectCtrl',['$scope', '$http', function($scope, $http){
-    $http.get('examples/home-markdown.md').then(function(response){
-      $scope.editor = {
-        markdown: response.data
-      };
-    });
+    $scope.editor = { markdown: "" };
   }]).
   controller('ThemesCtrl',['$scope', function($scope){
     $scope.setTheme = function(theme){
